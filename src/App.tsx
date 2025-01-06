907451e84162f3ddbbb58c9f41fd487b09ee8137
SundayNT <azterisk13@gmail.com>
2025-01-06 11:22:58 +0700

    4th commit separate conponent

--- src/App.tsx
+++ src/App.tsx
@@ -146,10 +146,68 @@
 // export default App;
 
 
+// import { useState } from "react";
+// import Popup from "./components/Popup";
+// import TaskList from "./components/TaskList";
+// import AddTaskButton from "./components/AddTaskButton";
+
+
+// function App() {
+//   const [isPopupOpen, setIsPopupOpen] = useState<boolean>(false);
+//   const [newTask, setNewTask] = useState(""); // Store new task input
+//   const [tasks, setTasks] = useState<string[]>([]); // Store task list
+
+//   // Open popup
+//   const openPopup = () => {
+//     setIsPopupOpen(true);
+//   };
+
+//   // Close popup
+//   const closePopup = () => {
+//     setIsPopupOpen(false);
+//   };
+
+//   // Add new task to the list
+//   const addTask = () => {
+//     if (newTask.trim()) {
+//       setTasks([...tasks, newTask]);
+//       setNewTask(""); // Clear input after adding task
+//       closePopup(); // Close popup after adding task
+//     }
+//   };
+
+//   return (
+//     <div className="relative flex min-h-screen items-center justify-center gap-5">
+//       {/* Popup Component */}
+//       <Popup
+//         isPopupOpen={isPopupOpen}
+//         closePopup={closePopup}
+//         newTask={newTask}
+//         setNewTask={setNewTask}
+//         addTask={addTask}
+//       />
+
+//       {/* Main Content */}
+//       <div className="mx-5 flex flex-col gap-5">
+//         {/* Add Task Button */}
+//         <AddTaskButton openPopup={openPopup} />
+
+//         {/* Task List Component */}
+//         <TaskList tasks={tasks} />
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
 import { useState } from "react";
 import Popup from "./components/Popup";
 import TaskList from "./components/TaskList";
 import AddTaskButton from "./components/AddTaskButton";
+
 
 function App() {
   const [isPopupOpen, setIsPopupOpen] = useState<boolean>(false);
@@ -199,3 +257,4 @@ function App() {
 }
 
 export default App;
+
